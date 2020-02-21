80e6cf9ad2c4cd7f0bb4c61cf90c6ea046fe6c22
Wils McCreight <30441468+WilsMcCreight@users.noreply.github.com>
2020-02-20 21:04:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os # To read files with
+import sys # For the exit command
 
 file_name = "./pi1000000.txt" # INPUT :: Insert the path to your own text file containing the number to scan
 starting_index = 1 # INPUT :: Insert the index you want our counting to begin with (The index of the tens place)
 
+if starting_index < 0 or int(starting_index) != startind_index:
+    print("Error: Invalid starting_index")
+    sys.exit()
+
 with open(file_name) as f: # Save the contents of the file to the variable 'digits'
-	digits = f.read()
+    digits = f.read()
 
 
 for i in range(len(digits)): # Strip the leading digits up until the decimal point, leaving just the decimal digits
